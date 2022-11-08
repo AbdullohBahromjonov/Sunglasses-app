a3c62524cf413c3653ee5444de073b7edbcbe5df
Abdulloh <abdullohbahromjonv@gmail.com>
2022-11-08 21:57:08 +0500

    Initial Commit

---
+++ Sunglasses_app/Sunglasses_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sunglasses_appApp.swift
+//  Sunglasses_app
+//
+//  Created by Abdulloh on 08/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Sunglasses_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
